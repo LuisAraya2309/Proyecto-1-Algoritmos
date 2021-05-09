2bbde43ce5ee35046d351ab5077a2f4a94403983
Luis Araya Mata <luistec2309@estudiantec.cr>
2021-05-09 17:55:01 -0600

    Arreglar error de rango en tendencias

--- main.cpp
+++ main.cpp
@@ -1,49 +1,148 @@
 #include<iostream>
 #include<opencv2/highgui/highgui.hpp>
 #include<vector>
+#include <unordered_map>
 
 using namespace std;
 using namespace cv;
 
 
-//uploadImageInfo performs the function of loading the base structure and all its information necessary to perform the search for the image
-void uploadImageInfo(vector<vector<string>> &vec){
-
-    string imagePath = "C:/Users/Sebastian/Pictures/Prueba2.jpg";
+void uploadImageOneInfo(vector<vector<string>> &pImageOneInfo){
+    /*
+    Purpose: Load the first image information and converts it to a matrix of colors for futures functionalities.
+    Paramateres: Recieves an empty matrix who is going to be filled with RGB colors.
+    Returns: Nothing. Void
+    */
+    string imagePath = "C:/Users/luist/OneDrive/Escritorio/Proyecto1/Prueba1.png";
     int blueChannel; int greenChannel; int redChannel;
     Mat colorImage; colorImage = imread(imagePath);
-
-    for (int imageColumns = 0; imageColumns < colorImage.rows; imageColumns++){
-        for (int imageRows = 0; imageRows < colorImage.cols; imageRows++) {
-            redChannel = colorImage.at<Vec3b>(imageColumns, imageRows)[2];
-            greenChannel = colorImage.at<Vec3b>(imageColumns, imageRows)[1];
-            blueChannel = colorImage.at<Vec3b>(imageColumns, imageRows)[0];
+    
+    for (int imageRows = 0; imageRows < colorImage.rows; imageRows+=54){
+        for (int imageColumns = 0; imageColumns < colorImage.cols; imageColumns++) {
+            redChannel = colorImage.at<Vec3b>(imageRows, imageColumns)[2];
+            greenChannel = colorImage.at<Vec3b>(imageRows, imageColumns)[1];
+            blueChannel = colorImage.at<Vec3b>(imageRows, imageColumns)[0];
 
             string rgbValue = to_string(redChannel) +","+ to_string(greenChannel)  +","+ to_string(blueChannel);
-            vec[imageColumns][imageRows] = rgbValue;
+            pImageOneInfo[imageRows][imageColumns] = rgbValue;
+            
         }
+       
     }
 }
 
-/*CreateDataHash performs the function of analyzing the tones of line segments extracted from the vectors,
-looks for a match with the second image and stores them in the hash if they match*/
-void createDataHash(){
+
+void uploadImageTwoInfo(vector<vector<string>> &pImageTwoInfo){
+    /*
+    Purpose: Load the second image information and converts it to a matrix of colors for futures functionalities.
+    Paramateres: Recieves an empty matrix who is going to be filled with RGB colors.
+    Returns: Nothing. Void
+    */
+
+    string imagePath = "C:/Users/luist/OneDrive/Escritorio/Proyecto1/Prueba2.jpg";
+    int blueChannel; int greenChannel; int redChannel;
+    Mat colorImage; colorImage = imread(imagePath);
     
+    for (int imageRows = 0; imageRows < colorImage.rows; imageRows+=54){
+        for (int imageColumns = 0; imageColumns < colorImage.cols; imageColumns++) {
+            redChannel = colorImage.at<Vec3b>(imageRows, imageColumns)[2];
+            greenChannel = colorImage.at<Vec3b>(imageRows, imageColumns)[1];
+            blueChannel = colorImage.at<Vec3b>(imageRows, imageColumns)[0];
+
+            string rgbValue = to_string(redChannel) +","+ to_string(greenChannel)  +","+ to_string(blueChannel);
+            pImageTwoInfo[imageRows][imageColumns] = rgbValue;
+            
+        }
+        
+    }
 }
 
-/*
-int *createSecondImageArrayTrend(){
-    const int imageRows = 3840 ;
-    const int imageColumns = 2160;
-    const int minimumCutoutSize = 108;
-    int arrayTrend[(imageColumns/(minimumCutoutSize/2))*3];
+void  selectTop3Trends(unordered_map<string, int> &colorAppearence,vector<int> &trendsPerLine,vector<string> &allTrends){
+    for(int trend = 0 ;trend < 3;trend++){
+
+        int trendColor = trendsPerLine[0];
+        int vectorSize = trendsPerLine.size();
+
+        for(int actualColor = 0;actualColor < vectorSize-1;actualColor++){
+            if(trendsPerLine[actualColor] < trendsPerLine[actualColor+1]){
+                trendColor = trendsPerLine[actualColor];
+            }
+        }
+
+        unordered_map<string, int>:: iterator hashColor;
+        string topTrend;
+        for(hashColor = colorAppearence.begin(); hashColor != colorAppearence.end(); hashColor++){
+            if(hashColor->second == trendColor){
+                topTrend = hashColor->first;
+                break;
+            }
+        }
+        colorAppearence.erase(topTrend);
+        allTrends.push_back(topTrend);
+
+    }
 }
-*/
+
+
+vector<string> createSecondImageArrayTrend(){
+
+    /*
+    Purpose: Create an array fulled with the trend colors from each 54 lines.
+    Parameters: Nothing
+    Returns: Pointer to the first value of the array.
+    */
+
+    const int rows = 40 ; const int columns = 3840; const int minimumCutoutSize = 108;
+
+    vector<vector<string>> imageTwoInfo( columns , vector<string> (rows));
+    uploadImageTwoInfo(imageTwoInfo);
+    cout<<"Segunda imagen cargada"<<endl;
+    vector<string> allTrends;
+    vector<int> trendsPerLine;
+    vector<string> top3TrendsPerLine;
+    
+    for(int lines = 0;lines<rows;lines++){
+        unordered_map<string, int> colorAppearence;
+        string actualColor;
+
+        for(int colors = 0;colors<columns;colors++){
+            actualColor = imageTwoInfo[lines][colors];
+            colorAppearence[actualColor]++;
+        }
+        unordered_map<string, int>:: iterator hashColor;
+        for (hashColor = colorAppearence.begin(); hashColor != colorAppearence.end(); hashColor++){
+            trendsPerLine.push_back(hashColor->second);
+        }
+        cout<<"Seleccionando top tres: "<<endl;
+        selectTop3Trends(colorAppearence,trendsPerLine,allTrends);
+        
+        cout<<"Las 120 tendencias: "<<endl;
+        for(int x=0;x<120;x++){
+            cout<<"Trend: "<<allTrends[x]<<endl;
+
+        }
+
+    }
+    return allTrends;
+
+}
+
+
+void createDataStructure(){
+    //const int rows = 40 ; const int columns = 3840;
+    //vector<vector<string>> imageOneInfo( rows , vector<string> (columns));
+    //uploadImageOneInfo(imageOneInfo);
+    cout<<"Iniciar el proceso"<<endl;
+    vector<string> allTrends = createSecondImageArrayTrend();
+
+}
+
+
+
+
 
 int main() {
-    const int rows = 3840 ; const int columns = 2160;
-    vector<vector<string>> vec( columns , vector<string> (rows));
-    uploadImageInfo(vec);
+    createDataStructure();
 
     return 0;
 }
